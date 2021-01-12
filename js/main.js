f79f6b598d9e487531057c1a1e78cdbce70ee681
Phillips III <1148547030121005@mil>
2021-01-12 14:42:31 -0500

    added copyright

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/* 
+ * Author: Ernie Phillips 
+ * Date: 01/12/2021
+ * Bellevue University
+ * CIS 515: IT Infrastructure
+ * Professor Krohn
+ */
+
+var date = new Date();
+$("#lblCurrentYear").text(date.getFullYear());
