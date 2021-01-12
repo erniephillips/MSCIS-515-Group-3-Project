7f8fd414026fe63fd99346b78c74248ca437f206
Ernie Phillips <eraphillips@my365.bellevue.edu>
2021-01-12 18:10:16 -0500

    added slow scroll

--- js/main.js
+++ js/main.js
@@ -6,5 +6,17 @@
  * Professor Krohn
  */
 
+//set current year in var in store in text in DOM
 var date = new Date();
 $("#lblCurrentYear").text(date.getFullYear());
+
+//set scroll to time lapse of 1 second
+$(function(){
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+    
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1000);
+    });
+});
