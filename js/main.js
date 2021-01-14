478a5d6a5162f6c5bd6669c5f8c22220e509b77d
Phillips III <1148547030121005@mil>
2021-01-14 08:24:29 -0500

    adjusted main image bounce discovered in IE

--- js/main.js
+++ js/main.js
@@ -8,32 +8,33 @@
 
 //set current year in var in store in text in DOM
 var date = new Date();
-$("#lblCurrentYear").text(date.getFullYear());
+$('#lblCurrentYear').text(date.getFullYear());
 
 //set scroll to time lapse of 1 second
-$(function(){
-    $(document).on('click', 'a[href^="#"]', function (event) {
-        event.preventDefault();
-    
-        $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
-        }, 1000);
-    });
+$(function() {
+	$(document).on('click', 'a[href^="#"]', function(event) {
+		event.preventDefault();
+
+		$('html, body').animate(
+			{
+				scrollTop: $($.attr(this, 'href')).offset().top
+			},
+			1000
+		);
+	});
 });
 
-$(window).scroll(function() {
-  
-    // get the variable of how far we've scrolled from the top
-    var offset = $(window).scrollTop();
-      offset     = offset * .7;
-  
-    // add css transform with the offset variable
-    $('#center-img-div').css({
-      '-moz-transform': 'rotate(' + offset + 'deg)',
-      '-webkit-transform': 'rotate(' + offset + 'deg)',
-      '-o-transform': 'rotate(' + offset + 'deg)',
-      '-ms-transform': 'rotate(' + offset + 'deg)',
-      'transform': 'rotate(' + offset + 'deg)',
-    });
-    
-  });
+$('body').scroll(function() {
+	// get the variable of how far we've scrolled from the top
+	var offset = $('body').scrollTop();
+	offset = offset * 0.7;
+
+	// add css transform with the offset variable
+	$('#center-img-div').css({
+		'-moz-transform': 'rotate(' + offset + 'deg)',
+		'-webkit-transform': 'rotate(' + offset + 'deg)',
+		'-o-transform': 'rotate(' + offset + 'deg)',
+		'-ms-transform': 'rotate(' + offset + 'deg)',
+		transform: 'rotate(' + offset + 'deg)'
+	});
+});
